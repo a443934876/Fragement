3a381281b78e3bc2df563618dd328f6722582434
a443934876 <378163883@qq.com>
2017-08-10 15:46:25 +0800

    更新

--- 12_数据库的创建/src/main/java/com/example/a12_/MainActivity.java
+++ 12_数据库的创建/src/main/java/com/example/a12_/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //这一句代码执行完毕数据是不会被创建的
+        MyDataBaseOpenHelper helper=new MyDataBaseOpenHelper(this);
+        //下面这一行代码执行了 数据库才会被创建
+        helper.getWritableDatabase();
+
     }
 }
--- 12_数据库的创建/src/main/java/com/example/a12_/MyDataBaseOpenHelper.java
+++ 12_数据库的创建/src/main/java/com/example/a12_/MyDataBaseOpenHelper.java
@@ -4,20 +4,20 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by Administrator on 2017/8/10.
- */
+
 
 public class MyDataBaseOpenHelper extends SQLiteOpenHelper {
 /**
  * @param context 上下文
- * @param name 数据库文件的名称
- * @param factory 默认游标工厂 从数据库文件的头部开始的
- * @param version 数据库的版本号 最小是1
+ *  test.db 数据库文件的名称
+ *  factory 默认游标工厂 从数据库文件的头部开始的
+ *  version 数据库的版本号 最小是1
  * */
-    public MyDataBaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public MyDataBaseOpenHelper(Context context) {
         super(context, "test.db",null, 1);
     }
+
+
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
